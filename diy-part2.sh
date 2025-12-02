54f039e44d57759b6a51853c585e6e2198384832
Gurosuogen <36596806+Gurosuogen@users.noreply.github.com>
2025-12-02 14:57:58 +0800

    Update IP, hostname, and enable USB power for Cudy TR3000

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,13 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Cudy-Tr3000/g' package/base-files/files/bin/config_generate
+
+# Enable USB power for Cudy TR3000 by default
+sed -i '/modem-power/,/};/{s/gpio-export,output = <1>;/gpio-export,output = <0>;/}' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1.dtsi
